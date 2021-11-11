9be0e663a25bb12e5e7aa48a1327a471f868e3c9
jsmithb117 <jsmithb117@gmail.com>
2021-11-11 11:05:20 -0800

    Capitalizes full names

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,6 +1,9 @@
 const Student = (props) => {
   const gradesInts = props.student.grades.map((grade) => parseInt(grade));
   const average = gradesInts.reduce((a, b) => a + b) / gradesInts.length;
+  const fullname = `${props.student.firstName} ${props.student.lastName}`.toUpperCase();
+
+  console.log('fullname: ', fullname)
   return (
     <div className="student">
       <img
@@ -8,7 +11,7 @@ const Student = (props) => {
         src={props.student.pic}
         alt={`user ${props.student.firstName} ${props.student.lastName}`}
       ></img>
-      <div className="fullname">{`${props.student.firstName} ${props.student.lastName}`}</div>
+      <div className="fullname">{fullname}</div>
       <div className="email">{`Email: ${props.student.email}`}</div>
       <div className="company">{`Company: ${props.student.company}`}</div>
       <div className="skill">{`Skill: ${props.student.skill}`}</div>
