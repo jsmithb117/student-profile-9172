83a36007e44def756d0689cef56a4c61a15a545f
jsmithb117 <jsmithb117@gmail.com>
2021-11-11 13:48:16 -0800

    Implements smooth-scrollbar-react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import dummyData from './data/data';
 import Student from './components/Student';
 import './App.css';
 
+import { Scrollbar } from 'smooth-scrollbar-react';
+
 function App() {
+  const scrollbar = useRef(null);
+
   useEffect(() => {
     const dataURL = "https://api.hatchways.io/assessment/students";
     fetch(dataURL)
       .then((response) => response.json())
       .then((json) => { setStudents(json.students) })
       .catch((err) => console.error(err));
-  }, []);
+  });
 
   const [students, setStudents] = useState(dummyData);
   return (
-    <div className="App" >
-      {students.map((student) => <Student key={student.firstName.concat(student.lastName)} student={student} />)}
+    <div className="scroll-outer">
+
+      <Scrollbar
+        ref={scrollbar}
+        alwaysShowTracks={false}
+        thumbMinSize={0}
+      >
+        <div className="scroll-container">
+          <div className="App" >
+            {students.map((student) => <Student key={student.firstName.concat(student.lastName)} student={student} />)}
+          </div>
+        </div>
+      </Scrollbar>
     </div>
+
   );
 }
 
--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,12 +1,13 @@
 const Student = (props) => {
   const gradesInts = props.student.grades.map((grade) => parseInt(grade));
   const average = gradesInts.reduce((a, b) => a + b) / gradesInts.length;
-  const fullname = `${props.student.firstName} ${props.student.lastName}`.toUpperCase();
+  const fullname =
+    `${props.student.firstName} ${props.student.lastName}`.toUpperCase();
 
   return (
     <div className="student">
       <img
-      className="image"
+        className="image"
         src={props.student.pic}
         alt={`user ${props.student.firstName} ${props.student.lastName}`}
       ></img>
@@ -14,9 +15,7 @@ const Student = (props) => {
       <div className="email">{`Email: ${props.student.email}`}</div>
       <div className="company">{`Company: ${props.student.company}`}</div>
       <div className="skill">{`Skill: ${props.student.skill}`}</div>
-      <div className="average">
-        {`${average}%`}
-      </div>
+      <div className="average">{`Average: ${average}%`}</div>
     </div>
   );
 };
